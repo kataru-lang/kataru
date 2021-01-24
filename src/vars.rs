fda13631a806d3df16096d0e424cd49cc8b01cfa
Josiah Putman <joshikatsu@gmail.com>
2021-01-24 11:48:23 -0800

    Supporting . syntax in var replacement

--- src/vars.rs
+++ src/vars.rs
@@ -5,7 +5,7 @@ use std::borrow::Cow;
 /// This is a line with var=${var} and var2=${var2}
 pub fn replace_vars(text: &str, bookmark: &Bookmark) -> String {
     lazy_static! {
-        static ref VARS_RE: Regex = Regex::new(r"\$\{([:a-zA-Z0-9_]*)\}").unwrap();
+        static ref VARS_RE: Regex = Regex::new(r"\$\{([:a-zA-Z0-9_\.]*)\}").unwrap();
     }
     VARS_RE
         .replace_all(&text, |cap: &Captures| {
@@ -35,13 +35,14 @@ mod tests {
         bookmark.state.insert("".to_string(), Map::new());
         let root_state = bookmark.state.get_mut("").unwrap();
         root_state.insert("var2".to_string(), Value::String("a".to_string()));
+        root_state.insert("char.var1".to_string(), Value::String("b".to_string()));
 
         assert_eq!(
             replace_vars(
-                "var1 = ${var1} and var2 = ${:var2}. This costs $10.",
+                "var1 = ${var1}, var2 = ${:var2}, char.var1 = ${char.var1}. Tickets cost $10.",
                 &bookmark
             ),
-            "var1 = 1 and var2 = a. This costs $10."
+            "var1 = 1, var2 = a, char.var1 = b. Tickets cost $10."
         )
     }
 
